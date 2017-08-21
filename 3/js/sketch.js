9eff3902b2730ad999a0815949f4f71156666f3e
Pankaj Uchil <puchil@Hush.internal>
2017-08-21 15:59:48 -0400

    update2D

--- 3/js/sketch.js
+++ 3/js/sketch.js
@@ -12,22 +12,27 @@ function setup() {
 }
 
 function draw(){
-    background(50);
-    stroke(255);
-    noFill();
-    beginShape();
-    let yOff = start;
+    let yOff = 0;
 
+    loadPixels();
+    
     for(let x=0; x<width; x++){
-        stroke(255);
-        let y = noise(yOff)*height;
-        vertex(x, y);
-        yOff += inc;
+        let xOff = 0;
+        for(let y=0; y<height; y++){
+        let index = ( x+y*width)*4;
+        let r = noise(xOff, yOff)*255;
 
+        pixels[ index + 0 ] = r;
+        pixels[ index + 1 ] = r;
+        pixels[ index + 2 ] = r;
+        pixels[ index + 3 ] = 255;   
+
+        xOff += inc;   
+     }
+     yOff += inc
     }
 
-    endShape();
-    start += inc;
+    updatePixels();
     //noLoop();
 /*
     let x = map(noise(xOff1), 0, 1, 0, width);
